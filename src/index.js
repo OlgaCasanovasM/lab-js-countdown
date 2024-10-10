ed6ab3d8f5e517790c300c5e0c4062b28c11acfe
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-10 20:59:17 +0200

    lab countdown w2d4 Olga

--- src/index.js
+++ src/index.js
@@ -2,37 +2,42 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 
-
-
+const buttonStart = document.getElementById("start-btn");
+buttonStart.addEventListener("click", startCountdown);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
+  //console.log("startCountdown called!");
 
   // Your code goes here ...
+  timer = setInterval(function () {
+    if (remainingTime > 0) {
+      remainingTime--;
+      let timerNum = document.getElementById("time");
+      timerNum.innerText = `${remainingTime}`;
+    } else {
+      clearInterval(timer);
+      showToast();
+    }
+  }, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
-
   // Your code goes here ...
-
-
-
+  if ((remainingTime = 0)) {
+    const toastMessage = document.querySelector("div.toast");
+    toastMessage.setTimeout(() => {
+      toastMessage.classList.toggle("show");
+    }, 3000);
+  }
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
